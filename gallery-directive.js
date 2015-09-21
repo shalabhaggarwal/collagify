26b540a41bec3e1d2362088068c5a287280c9aaf
Shalabh Aggarwal <shalabh7777@gmail.com>
2015-09-21 19:16:47 +0000

    Collage application ready for demo.
    Fabric.js has been used to help with canvas utils

--- gallery-directive.js
+++ gallery-directive.js
@@ -1,21 +1,22 @@
-var galleryCtrl = function() {
+var galleryCtrl = function($scope) {
   this.images;
   
   this.getImages();
+
+  document.body.addEventListener("imageDeleted", function (event) {
+    this.images.push(event.detail.imageDeleted);
+  }.bind(this));
 };
+
 
 galleryCtrl.prototype.getImages = function() {
   this.images = [
     'images/wildlife-adventure-holidays.jpg',
     'images/wildlife_nature_06.jpg',
-    'images/wildlife_nature_06.jpg',
-    'images/wildlife_nature_06.jpg',
-    'images/wildlife_nature_06.jpg',
-    'images/wildlife_nature_06.jpg',
-    'images/wildlife_nature_06.jpg',
     'images/WildlifePhotography-11.jpg'
   ];
 };
+
 
 var galleryDirective = function() {
   return {
--- workspace-directive.js
+++ workspace-directive.js
@@ -1,17 +1,70 @@
-var workspaceCtrl = function() {
-  this.images = [];
+var workspaceCtrl = function($scope) {
+  this.droppedImages = [];
+
+  this.canvas = new fabric.Canvas('workspaceCanvas', {
+    hoverCursor: 'pointer'
+  });
+
+  var element = document.getElementById("workspaceCanvas");
+  element.fabric = this.canvas;
+  element.droppedImages = this.droppedImages;
+
+  this.canvas.on({
+    'object:moving': this.imageDragged,
+  });
+
+  document.onkeydown = this.onKeyDownHandler;
+};
+
+
+workspaceCtrl.prototype.onKeyDownHandler = function(event) {
+  var element = document.getElementById("workspaceCanvas");
+  var canvas = element.fabric;
+  switch (event.keyCode) {
+    case 46: // delete
+    case 8: // backspace (Mac has only one key)
+      event.preventDefault();
+      var activeImage = canvas.getActiveObject();
+      if (activeImage) {
+        canvas.remove(activeImage);
+      }
+      element.droppedImages.splice(
+        element.droppedImages.findIndex(function(image) {
+          return image.split('/').pop() == activeImage.getSvgSrc().split('/').pop();
+        }), 1
+      );
+      var imageDeletedEvent = new CustomEvent('imageDeleted', {
+        detail: {
+          imageDeleted: activeImage.getSvgSrc()
+        }
+      });
+      document.body.dispatchEvent(imageDeletedEvent);
+  }
+};
+
+
+workspaceCtrl.prototype.imageDragged = function (event) {
+  event.target.bringToFront();
 };
 
 
 workspaceCtrl.prototype.dropImage = function(event, ui) {
-  debugger;
+  var droppedImage = ui.helper.context.currentSrc;
+  fabric.Image.fromURL(droppedImage, function(oImg) {
+    this.wsCtrl.canvas.add(oImg);
+  }.bind(this));
 };
+
+
+workspaceCtrl.prototype.saveCollage = function() {
+  this.canvas.deactivateAll().renderAll();
+  window.open(this.canvas.toDataURL('png'));
+};
+
 
 var workspaceDirective = function() {
   return {
-    scope: {
-      images: '='
-    },
+    scope: {},
     templateUrl: 'workspace.html',
     controller: workspaceCtrl,
     controllerAs: 'wsCtrl',
